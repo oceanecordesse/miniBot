0802800ef8526f3c712e3711437295b67846302a
oceanecordesse <38215591+oceanecordesse@users.noreply.github.com>
2018-05-04 10:46:42 +0200

    Update talkToMiniBot.js

--- javascript/talkToMiniBot.js
+++ javascript/talkToMiniBot.js
@@ -21,7 +21,7 @@ function talkToMiniBot() {
   // HTTP POST request for chatbot answer
   $.ajax({
   type: "POST",
-    url: "http://167.114.255.133:8888/minibot/api/msg",
+    url: "http://109.31.68.167:8888/minibot/api/msg",
     data: { msg: userInput },
     success: function ( data ) {
       botMsg = data["msg"];
@@ -38,7 +38,7 @@ function complainAboutAnswer() {
   // HTTP POST request for chatbot answer
   $.ajax({
   type: "POST",
-    url: "http://167.114.255.133:8888/minibot/api/complain",
+    url: "http://109.31.68.167:8888/minibot/api/complain",
     success: function ( data ) {
       formattedMsg = "<p>" + "I apologise, I will notice my developers." + "</p>";
       $("#botDiv").html(formattedMsg); // Update display with chatbot answer
